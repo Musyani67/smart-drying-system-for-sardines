7236f6483626ba61a1e9f497f8ea14fdfa2c45a7
Musy67 <smusyani67@gmail.com>
2025-08-12 17:01:18 +0300

    Written LCD code

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(0x27, 16, 2);
+
+// Function initialization
+void writeInLcd(int col, int row, String message);
+
+void setup() {
+  lcd.begin();
+  lcd.backlight();
+  writeInLcd(0, 0, "Starting...");
+  lcd.clear();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+// Functions
+void writeInLcd(int col, int row, String message) {
+  /*
+    If you want to print "Hello World", you write as follows:
+    writeInLcd(0, 0, "Hello World");
+  */
+  lcd.SetCursor(col, row);
+  lcd.print(message);
+
+  delay(1000); // In order to see the words displayed.
 }
